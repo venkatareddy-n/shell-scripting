7b9091d742af9aafa567afdf63a688e8a298c991
venkatswan1991 <venkatswan1991@gmail.com>
2025-06-22 15:53:18 +0530

    shell

---
+++ 6-R6.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Reading input and timeout
+read -p "Enter your name: " name
+
+read -t 10 "Enter your password in 10 seconds: " password
+
+echo "Hello $name your password is: $password"
